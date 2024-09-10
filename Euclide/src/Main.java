7ccc4ed7cc5febca5738903222a1fc42f5ad3733
LamAlexandre <155826255+LamAlexandre@users.noreply.github.com>
2024-09-10 11:58:04 +0200

    Add files via upload

--- Euclide/src/Main.java
+++ Euclide/src/Main.java
@@ -1,17 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
-        }
+    public static void main(String[] args) {
+        int a = 33;
+        int b = 54;
+        int c = pgcd(a,b);
+        system.out.println("c");
     }
-    public static pgcd(int a, int b){
+    public static int pgcd(int a, int b){
     while(b!=a){
         if(a>b){
             a=a-b;
         }
         else{
-            b=b_a;
+            b=b-a;
         }
     }
     return a;
